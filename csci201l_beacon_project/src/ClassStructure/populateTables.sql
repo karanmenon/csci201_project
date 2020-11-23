9794e081a4f54c79e7d8cc915428758991e2d09a
Erin Bristow <eebristo@usc.edu>
2020-11-22 20:55:16 -0800

    populate!

---
+++ csci201l_beacon_project/src/ClassStructure/populateTables.sql
@@ -0,0 +1,8 @@
+INSERT INTO Comments (userID, postID, commentContent, timeStamps) values
+	(12,1,'I can help! Just sent an email', '2020-11-22 09:00:00'),
+	(8,2, 'Thanks for the heads up, was just about to travel that way', '2020-11-22 09:00:00'),
+	(10,4, 'I think the high school still has some spots', '2020-11-22 09:00:00'),
+	(9,17, 'thanks for this list','2020-11-22 08:00:00'),
+	(13,17, 'Sorry about the loss your community is facing right now','2020-11-22 08:55:00')
+	
+;
